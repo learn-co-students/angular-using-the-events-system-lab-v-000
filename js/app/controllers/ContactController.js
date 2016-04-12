c0dd20e8ccce80c2a6769fe0e8a005f388551edc
alexbarron <alexbarron@gmail.com>
2016-04-12 11:32:11 -0700

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
 function ContactController($rootScope) {
-
+    var ctrl = this;
+    $rootScope.$on('remove', function(event, id){
+        ctrl.contacts.splice(id, 1);
+    });
     this.contacts = [
         {
             "name": {
