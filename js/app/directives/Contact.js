9d5899df9c28563b420e66ab05b3b8b93da3dc37
Chris-Sexton <champlain2013@gmail.com>
2016-10-11 14:54:33 -0400

    Done.

--- js/app/directives/Contact.js
+++ js/app/directives/Contact.js
@@ -8,7 +8,9 @@ function Contact() {
 		].join(''),
 		controller: function ($rootScope) {
 			this.remove = function (id) {
-				
+
+				$rootScope.$broadcast('remove', id)
+
 			};
 		},
 		controllerAs: 'ctrl',
